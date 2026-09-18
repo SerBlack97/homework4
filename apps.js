11ddca8fcd7f3f73751ddc3a26faced418cbd25d
SerBlack97 <serblack@iu.edu>
2026-09-18 14:24:45 -0400

    push

--- apps.js
+++ apps.js
@@ -3,6 +3,10 @@ const fnumber = Number(prompt("What is the first number?"));
 const snumber =  Number(prompt ("What is the second number?"));
 
 
+let start = "The gorilla ";
+let end = "on the moon.";
+
+const fill = prompt(`Fill in the blank: \n ${start} ____ ${end}`);
 
 // Date and Time
 const now = new Date();
@@ -13,5 +17,8 @@ const avg = (fnumber + snumber) / 2;
 // Outputs
 console.log(`The average of ${fnumber} and ${snumber} is ${avg}`);
 
+console.log(`${start}${fill} ${end}`);
+
 console.log(now.toLocaleString());
+
 
